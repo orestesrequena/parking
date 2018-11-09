ec7407c3b61bf8bb060aeadebfac65af9028d896
Mickael C <mickaelchapoy@gmail.com>
2018-11-09 11:05:46 +0100

    design list

--- www/js/parkings.js
+++ www/js/parkings.js
@@ -1,5 +1,5 @@
 function getParkings(virtualList) {
-    console.log('parking');
+    // console.log('parking');
     $.ajax({
         url: "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=disponibilite-parkings&facet=libelle&facet=ville&facet=etat",
         method: "get",
@@ -7,8 +7,20 @@ function getParkings(virtualList) {
         success: function(json) {
                         
             for (var i = 0; i < json.records.length; i++) {
-                var disponibilite = json.records[i].fields.dispo > 0 ? "dispo.jpg" : "nondispo.jpg";
+                var disponibilite = json.records[i].fields.dispo;
+                // var el = document.getElementsByClassName('material-icons');
 
+                // switch (disponibilite) {
+                //     case (disponibilite > 15):
+                //         el.className += " green";
+                //         break;
+                //     case (disponibilite < 15 && disponibilite > 0):
+                //         el.className += " orange";
+                //         break;
+                //     case (disponibilite === 0):
+                //         el.className += " red";
+                //         break;
+                // }
 
                 virtualList.appendItem({
                     records: json.records[i],
